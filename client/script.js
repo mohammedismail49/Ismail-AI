18ed260db8a4bbad871b6529d5480cd76cc432bf
mohammedismail49 <112697953+mohammedismail49@users.noreply.github.com>
2023-01-13 18:34:00 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -99,7 +99,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://first-ai.onrender.com', {
+    const response = await fetch('https://openai-ismail12.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
